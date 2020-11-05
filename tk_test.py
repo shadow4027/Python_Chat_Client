90fc616f22cce9f28b9373d4939d19869284bfde
FuruiKami <sylverstarlight@gmail.com>
2020-11-05 03:47:47 -0700

    tk test

--- tk_test.py
+++ tk_test.py
@@ -8,14 +8,14 @@ class Window(Frame):
         self.master = master
         self.init_window(title)
 
+    def client_exit(self):
+        exit()
+
     def init_window(self, title):
         self.master.title(title)
         self.pack(fill=BOTH, expand=1)
         quit_button = Button(self, text="Quit", command=self.client_exit)
         quit_button.place(x=366, y=0)
-
-    def client_exit(self):
-        exit()
 
 
 root = Tk()
